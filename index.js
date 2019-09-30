b65e39946ac9996fbe25a47414efb7f93a9858e7
Ana <a.vargas.de.noronha@accenture.com>
2019-09-30 15:54:22 +0100

    Adding LaunchRequest

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ exports.handler = function(event, context) {
   var request = event.request;
 
   if (request.type === "LaunchRequest") {
+    let options = {};
+    options.speechText =
+      "Welcome to greeting skill. Using our skill you can greet your guests. Whom you want to greet?";
+    options.reprompt = "You can say for example, say hello to John";
+    options.endSession = false;
+    context.succeed(buildResponse(options));
   } else if (request.type === "IntentRequest") {
   } else if (request.type === "SessionEndedRequest") {
   } else {
