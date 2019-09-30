0b0d0b6e0797960a6fbf6cdd20d24ccbe037f9ac
Ana <a.vargas.de.noronha@accenture.com>
2019-09-30 15:41:47 +0100

    Adding three type of requests

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-exports.handler = function(event, context) {};
+exports.handler = function(event, context) {
+  var request = event.request;
+
+  if (request.type === "LaunchRequest") {
+  } else if (request.type === "IntentRequest") {
+  } else if (request.type === "SessionEndedRequest") {
+  } else {
+  }
+};
